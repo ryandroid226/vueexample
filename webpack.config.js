9aae1c5a405c0319384d1fd9e6bb950382d69a24
Ryan Holt <rholt@getuwired.com>
2022-01-28 13:59:39 -0500

    Adds webpack and vue

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+const path = require('path');
+const isDev = (process.env.NODE_ENV = 'development')
+
+module.exports = {
+    entry: {
+        public: [
+            path.resolve(__dirname, './public/js/public.js'),
+            path.resolve(__dirname, './public/js/somevuepage/somevuepage.js') //Example of extra vue js being added in
+        ],
+        admin: path.resolve(__dirname, './admin/js/admin.js')
+    },
+    devtool: isDev ? 'inline-source-map' : false,
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    output: {
+        filename: '[name]/js/vueexample_[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
